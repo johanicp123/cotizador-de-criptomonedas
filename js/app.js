0cc6ee4c98d7473e19c7ffa4c7ea3c19bf08b550
johanicp123 <johanicp123@gmail.com>
2022-05-04 01:23:40 -0500

    javascript
    comparto javascript de practica API de cotizador de criptomonedas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const formulario = document.querySelector('#formulario');
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const monedaSelect = document.querySelector('#moneda');
+const resultado = document.querySelector('#resultado');
 
 const objBusqueda = {
     moneda: '',
@@ -55,6 +56,9 @@ function submitFormulario(e){
         mostrarAlerta('Ambos campos son obligatorios');
         return;
     }
+
+    //consultar la API con los resultados
+    consultarAPI();
 }
 
 function mostrarAlerta (msg){
@@ -73,4 +77,66 @@ function mostrarAlerta (msg){
             divMensaje.remove();
         },3000);
     }
+}
+
+function consultarAPI(){
+    const { moneda, criptomoneda} = objBusqueda;
+    const url =`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then( cotizacion =>{
+            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+        })
+
+}
+
+function mostrarCotizacionHTML(cotizacion){
+    limpiarHTML();
+
+    const { PRICE,HIGHDAY,LOWDAY,CHANGEPCT24HOUR,LASTUPDATE} = cotizacion;
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span> ${PRICE} </span>`;
+
+    const precioAlto =document.createElement('p');
+    precioAlto.innerHTML = `<p>Precio màs alto del dia <span>${HIGHDAY}</span>`;
+
+    const precioBajo =document.createElement('p');
+    precioBajo.innerHTML = `<p>Precio màs bajo del dia <span>${LOWDAY}</span>`;
+
+    const ultimasHoras =document.createElement('p');
+    ultimasHoras.innerHTML = `<p>Variacion ultimas 24 horas <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const ultimaActualizacion =document.createElement('p');
+    ultimaActualizacion.innerHTML = `<p>Ultima actualizaciòn <span>${LASTUPDATE}</span>`;
+    
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpinner(){
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML =`
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `;
+
+    resultado.appendChild(spinner);
 }
